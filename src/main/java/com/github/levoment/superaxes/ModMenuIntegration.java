31041c775b680f740cd6a928861cfd8fcc29c5a4
Levoment <30224138+Levoment@users.noreply.github.com>
2022-05-18 18:28:59 -0500

    Update to 1.19-pre1

--- src/main/java/com/github/levoment/superaxes/ModMenuIntegration.java
+++ src/main/java/com/github/levoment/superaxes/ModMenuIntegration.java
@@ -2,15 +2,13 @@ package com.github.levoment.superaxes;
 
 import com.terraformersmc.modmenu.api.ConfigScreenFactory;
 import com.terraformersmc.modmenu.api.ModMenuApi;
-
-
 import me.shedaniel.clothconfig2.api.ConfigBuilder;
 import me.shedaniel.clothconfig2.api.ConfigCategory;
 import me.shedaniel.clothconfig2.api.ConfigEntryBuilder;
 import net.minecraft.client.MinecraftClient;
 import net.minecraft.text.Style;
 import net.minecraft.text.Text;
-import net.minecraft.text.TranslatableText;
+import net.minecraft.text.TranslatableTextContent;
 
 import java.util.Properties;
 
@@ -23,50 +21,53 @@ public class ModMenuIntegration implements ModMenuApi {
             // Get the previous screen
             ConfigBuilder builder = ConfigBuilder.create()
                     .setParentScreen(MinecraftClient.getInstance().currentScreen)
-                    .setTitle(new TranslatableText("configuration.superaxes.config"));
+                    .setTitle(Text.translatable("configuration.superaxes.config"));
 
 
             // Set category
-            ConfigCategory general = builder.getOrCreateCategory(new TranslatableText("axebehaviour.superaxes.general"));
+            ConfigCategory general = builder.getOrCreateCategory(Text.translatable("axebehaviour.superaxes.general"));
 
             // Set an option for harvesting leaves
             ConfigEntryBuilder entryBuilder = builder.entryBuilder();
-            general.addEntry(entryBuilder.startBooleanToggle(new TranslatableText("option.superaxes.harvest_leaves"), SuperAxesMod.harvestLeaves)
+            general.addEntry(entryBuilder.startBooleanToggle(Text.translatable("option.superaxes.harvest_leaves"), SuperAxesMod.harvestLeaves)
                     .setDefaultValue(false)
-                    .setTooltip(new TranslatableText("option.superaxes.harvest_leaves.tooltip"))
+                    .setTooltip(Text.translatable("option.superaxes.harvest_leaves.tooltip"))
                     .setSaveConsumer(newValue -> SuperAxesMod.harvestLeaves = newValue)
                     .build());
 
             // Set an option for the range
-            general.addEntry(entryBuilder.startIntField (new TranslatableText("option.superaxes.range"), SuperAxesMod.range)
+            general.addEntry(entryBuilder.startIntField (Text.translatable("option.superaxes.range"), SuperAxesMod.range)
                     .setDefaultValue(5)
-                    .setTooltip(new TranslatableText("option.superaxes.range.tooltip"))
+                    .setTooltip(Text.translatable("option.superaxes.range.tooltip"))
                     .setSaveConsumer(newValue -> SuperAxesMod.range = newValue)
                     .build());
 
+
+
             // Set an option for limiting the log radius search
-            general.addEntry(entryBuilder.startBooleanToggle(new TranslatableText("option.superaxes.limit_log_search"), SuperAxesMod.limitSearch)
+            general.addEntry(entryBuilder.startBooleanToggle(Text.translatable("option.superaxes.limit_log_search"), SuperAxesMod.limitSearch)
                     .setDefaultValue(false)
-                    .setTooltip(new TranslatableText("option.superaxes.limit_log_search.tooltip"))
+                    .setTooltip(Text.translatable("option.superaxes.limit_log_search.tooltip"))
                     .setSaveConsumer(newValue -> SuperAxesMod.limitSearch = newValue)
                     .build());
 
             // Set an option for the log search radius
-            general.addEntry(entryBuilder.startIntField (new TranslatableText("option.superaxes.log_radius"), SuperAxesMod.logRadius)
+            general.addEntry(entryBuilder.startIntField (Text.translatable("option.superaxes.log_radius"), SuperAxesMod.logRadius)
                     .setDefaultValue(8)
-                    .setTooltip(new TranslatableText("option.superaxes.log_radius.tooltip"))
+                    .setTooltip(Text.translatable("option.superaxes.log_radius.tooltip"))
                     .setSaveConsumer(newValue -> SuperAxesMod.logRadius = newValue)
                     .build());
 
 
             // The shape scale tooltip
             Text[] shapeScaleTexts = {
-                    new TranslatableText("option.superaxes.shape_scale").setStyle(Style.EMPTY.withBold(true)),
-                    new TranslatableText("option.superaxes.shape_scale.tooltip"),
-                    new TranslatableText("option.superaxes.shape_scale.tooltip_2"),
-                    new TranslatableText("option.superaxes.shape_scale.tooltip_3")};
+                    Text.translatable("option.superaxes.shape_scale").getWithStyle(Style.EMPTY.withBold(true)).get(0),
+                    // Text.translatable("option.superaxes.shape_scale").setStyle(Style.EMPTY.withBold(true)),
+                    Text.translatable("option.superaxes.shape_scale.tooltip"),
+                    Text.translatable("option.superaxes.shape_scale.tooltip_2"),
+                    Text.translatable("option.superaxes.shape_scale.tooltip_3")};
             // Set an option for the shape scale
-            general.addEntry(entryBuilder.startIntField (new TranslatableText("option.superaxes.shape_scale"), SuperAxesMod.shapeScale)
+            general.addEntry(entryBuilder.startIntField (Text.translatable("option.superaxes.shape_scale"), SuperAxesMod.shapeScale)
                     .setDefaultValue(1)
                     .setTooltip(shapeScaleTexts)
                     .setSaveConsumer(newValue -> SuperAxesMod.shapeScale = newValue)
@@ -75,13 +76,14 @@ public class ModMenuIntegration implements ModMenuApi {
 
             // The shape show debug lines tooltip
             Text[] showDebugLinesTexts = {
-                    new TranslatableText("option.superaxes.debug_lines").setStyle(Style.EMPTY.withBold(true)),
-                    new TranslatableText("option.superaxes.debug_lines.tooltip"),
-                    new TranslatableText("option.superaxes.debug_lines.tooltip_2"),
-                    new TranslatableText("option.superaxes.debug_lines.tooltip_3"),
-                    new TranslatableText("option.superaxes.debug_lines.tooltip_4")};
+                    Text.translatable("option.superaxes.debug_lines").getWithStyle(Style.EMPTY.withBold(true)).get(0),
+//                    Text.translatable("option.superaxes.debug_lines").setStyle(Style.EMPTY.withBold(true)),
+                    Text.translatable("option.superaxes.debug_lines.tooltip"),
+                    Text.translatable("option.superaxes.debug_lines.tooltip_2"),
+                    Text.translatable("option.superaxes.debug_lines.tooltip_3"),
+                    Text.translatable("option.superaxes.debug_lines.tooltip_4")};
             // Set an option for showing bounding lines of blocks that will be broken
-            general.addEntry(entryBuilder.startBooleanToggle(new TranslatableText("option.superaxes.debug_lines"), SuperAxesMod.showDebugLines)
+            general.addEntry(entryBuilder.startBooleanToggle(Text.translatable("option.superaxes.debug_lines"), SuperAxesMod.showDebugLines)
                     .setDefaultValue(false)
                     .setTooltip(showDebugLinesTexts)
                     .setSaveConsumer(newValue -> SuperAxesMod.showDebugLines = newValue)
@@ -89,13 +91,13 @@ public class ModMenuIntegration implements ModMenuApi {
 
             // The shape show debug highlight tooltip
 //            Text[] showDebugHighlightText = {
-//                    new TranslatableText("option.superaxes.debug_highlight").setStyle(Style.EMPTY.withBold(true)),
-//                    new TranslatableText("option.superaxes.debug_highlight.tooltip"),
-//                    new TranslatableText("option.superaxes.debug_highlight.tooltip_2"),
-//                    new TranslatableText("option.superaxes.debug_highlight.tooltip_3"),
-//                    new TranslatableText("option.superaxes.debug_highlight.tooltip_4")};
+//                    Text.translatable("option.superaxes.debug_highlight").setStyle(Style.EMPTY.withBold(true)),
+//                    Text.translatable("option.superaxes.debug_highlight.tooltip"),
+//                    Text.translatable("option.superaxes.debug_highlight.tooltip_2"),
+//                    Text.translatable("option.superaxes.debug_highlight.tooltip_3"),
+//                    Text.translatable("option.superaxes.debug_highlight.tooltip_4")};
 //            // Set an option for showing bounding box of blocks that will be broken
-//            general.addEntry(entryBuilder.startBooleanToggle(new TranslatableText("option.superaxes.debug_highlight"), SuperAxesMod.showDebugHighlight)
+//            general.addEntry(entryBuilder.startBooleanToggle(Text.translatable("option.superaxes.debug_highlight"), SuperAxesMod.showDebugHighlight)
 //                    .setDefaultValue(false)
 //                    .setTooltip(showDebugHighlightText)
 //                    .setSaveConsumer(newValue -> SuperAxesMod.showDebugHighlight = newValue)
